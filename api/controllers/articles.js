7055606b1761d9830ed9f5e20bce565e69fcf955
Ohad Cohen <ohad909881122@gmail.com>
2022-09-20 15:22:49 +0300

    U

---
+++ api/controllers/articles.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose");
+const Article = require("../models/article");
+
+module.exports = {
+  getAllArticles: (req, res) => {
+    Article.find()
+      .then((articles) => {
+        res.status(200).json({
+          articles,
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          error,
+        });
+      });
+  },
+
+  createArticle: (req, res) => {
+    const { title, description, content } = req.body;
+
+    const article = new Article({
+      _id: new mongoose.Types.ObjectId(),
+      title,
+      description,
+      content,
+    });
+
+    article
+      .save()
+      .then(() => {
+        res.status(200).json({
+          message: "Created article",
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          error,
+        });
+      });
+
+    res.status(200).json({
+      message: "Create a new article",
+    });
+  },
+  getArticle: (req, res) => {
+    const articleId = req.params.articleId;
+
+    Article.findById(articleId)
+      .then((article) => {
+        res.status(200).json({
+          article,
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          error,
+        });
+      });
+  },
+  updateArticle: (req, res) => {
+    const articleId = req.params.articleId;
+
+    Article.updateOne({ _id: articleId }, req.body).then(() => {
+      res
+        .status(200)
+        .json({
+          message: "Article Updated",
+        })
+        .catch((error) => {
+          res.status(500).json({
+            error,
+          });
+        });
+    });
+  },
+
+  deleteArticle: (req, res) => {
+    const articleId = req.params.articleId;
+
+    Article.remove({ _id: articleId })
+      .then(() => {
+        res.status(200).json({
+          message: `Article _id :  ${articleId} Deleted`,
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          error,
+        });
+      });
+  },
+};
---
+++ api/routes/articles.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    getAllArticles,
+    createArticle,
+    getArticle,
+    updateArticle,
+    deleteArticle
+} = require('../controllers/articles');
+
+router.get('/',getAllArticles);
+router.post('/',createArticle);
+router.get('/:articleId',getArticle)
+router.patch('/:articleId',updateArticle);
+router.delete('/:articleId',deleteArticle);
+
+
+module.exports = router;
